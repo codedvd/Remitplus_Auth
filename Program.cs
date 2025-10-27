fedc4fee3a22754503a503604ec47b6277867f4f
codedvd <victorochez@outlook.com>
2025-10-27 13:26:21 +0100

    updates

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Remitplus_Authentication.Middlewares;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
 // Configure Serilog
 Log.Logger = new LoggerConfiguration()
     .WriteTo.File(
@@ -10,46 +11,25 @@ Log.Logger = new LoggerConfiguration()
         outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
     )
     .CreateLogger();
+
 builder.Host.UseSerilog();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Serve static files (your React build)
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.UseCors("AllowAll");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-}
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHealthChecks("/health");
-app.UseHttpsRedirection();
-
-app.MapWhen(
-    context => context.Request.Path.StartsWithSegments("/api"),
-    apiApp =>
-    {
-        app.UseMiddleware<JwtValidationMiddleware>();
-        app.UseMiddleware<ApiKeyMiddleware>();
-
-        apiApp.UseRouting();
-        apiApp.UseAuthorization();
-        apiApp.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
-    }
-);
 
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
